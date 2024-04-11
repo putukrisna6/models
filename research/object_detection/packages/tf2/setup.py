f6f6aa4cfe3ef3b644820a34e1774e9ff3dadf0a
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2024-04-11 21:31:26 +0700

    fix(tf2 setup): keras error

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -18,9 +18,8 @@ REQUIRED_PACKAGES = [
     'lvis',
     'scipy',
     'pandas',
-    'tf-models-official>=2.5.1',
+    'tf-models-official>=2.5.1,<2.16.0',
     'tensorflow_io',
-    'keras',
     'pyparsing==2.4.7',  # TODO(b/204103388)
     'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
 ]
