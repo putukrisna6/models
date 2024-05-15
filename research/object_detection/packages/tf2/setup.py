09ffa625e8b1575b0c98cf149fe1bdaf46f101ed
Putu Krisna Andyartha <43901559+putukrisna6@users.noreply.github.com>
2024-05-15 20:18:43 +0700

    revert

--- research/object_detection/packages/tf2/setup.py
+++ research/object_detection/packages/tf2/setup.py
@@ -18,7 +18,7 @@ REQUIRED_PACKAGES = [
     'lvis',
     'scipy',
     'pandas',
-    'tf-models-official>=2.5.1',
+    'tf-models-official>=2.5.1,<2.16.0',
     'tensorflow_io',
     'pyparsing==2.4.7',  # TODO(b/204103388)
     'sacrebleu<=2.2.0'  # https://github.com/mjpost/sacrebleu/issues/209
